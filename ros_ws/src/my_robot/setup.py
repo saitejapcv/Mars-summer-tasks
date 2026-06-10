20fffb66cba48e44a1310a9a34673a451cfd6869
saitejapcv <charandeviswamy@gmail.com>
2026-06-10 16:13:10 +0530

    adding xacro

--- ros_ws/src/my_robot/setup.py
+++ ros_ws/src/my_robot/setup.py
@@ -13,7 +13,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'urdf'),
-         glob('urdf/*.urdf')),
+         glob('urdf/*.urdf') + glob('urdf/*.xacro')),
         (os.path.join('share', package_name, 'launch'),
          glob('launch/*.py')),
         (os.path.join('share', package_name, 'worlds'),
