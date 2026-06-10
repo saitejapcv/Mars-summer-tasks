260fbc719e14fa51a91efc93f9fb6f643721efd5
saitejapcv <charandeviswamy@gmail.com>
2026-06-10 16:34:06 +0530

    updating launch file for xacro

--- ros_ws/src/my_robot/launch/gazebo.launch.py
+++ ros_ws/src/my_robot/launch/gazebo.launch.py
@@ -4,6 +4,7 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
+import xacro
 
 def generate_launch_description():
 
