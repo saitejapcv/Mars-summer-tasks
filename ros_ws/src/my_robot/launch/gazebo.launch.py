bbf89dcc9299469ddc78cbe2c4b752d5415fc9f4
saitejapcv <charandeviswamy@gmail.com>
2026-06-11 16:13:37 +0530

    Fixing Bugs

--- ros_ws/src/my_robot/launch/gazebo.launch.py
+++ ros_ws/src/my_robot/launch/gazebo.launch.py
@@ -58,9 +58,6 @@ def generate_launch_description():
             '/cmd_vel@geometry_msgs/msg/Twist@ignition.msgs.Twist',
             '/scan@sensor_msgs/msg/LaserScan[ignition.msgs.LaserScan',
         ],
-        remappings=[
-            ('/model/my_robot/link/lidar_frame/sensor/lidar_sensor/scan', '/scan'),
-        ],
         output='screen'
     )
     
